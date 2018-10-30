8a790b50c71a215a2a56b566a49090aff6bd5ede
Jesus Nunez <jesus.nunez@incontact.com>
2018-10-30 17:27:08 -0400

    using binding and model locators

--- protractor/ratings/create.event.spec.js
+++ protractor/ratings/create.event.spec.js
@@ -15,7 +15,6 @@ describe('Adding a new event', function(){
         });       
     });
 
-
     describe('When specifying the name', function(){
         it('Should enable the Create button', function(){
             element(by.model('event.name')).sendKyes('A Sample Event');
@@ -23,6 +22,20 @@ describe('Adding a new event', function(){
             var css = button.getAttribute('class');
 
             expect(css).not.toMatch('disabled');
+        });
+    });
+
+    describe('When saving the form', function(){
+        it('Should add the event to the list', function(){
+            element(by.model('event.name')).sendKyes('Module 3');
+
+            button.click();
+
+            browser.waitForAngular();
+
+            var list = element.all(by.binding('name'));
+
+            expect(list.getText()).toMatch('Module 3')
         });
     });
 });
